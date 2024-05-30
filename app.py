8f6522b95d7ee17059e41d5b6814c6f1fa1db7a6
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-05-31 00:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-messages = list(messages_collection.find({}, {'_id': 0, 'username': 1, 'message': 1, 'time': 1, 'date': 1}))
-
 users_online = 0
 
-@app.route('/')
-def chat():
-    messages = list(messages_collection.find({}, {'_id': 0, 'username': 1, 'message': 1, 'time': 1, 'date': 1}))
-    
+# Store last message details in memory for quick access
+last_message_details = {
+    'username': None,
+    'time': None
+}
+
+def fetch_messages():
+    messages = list(messages_collection.find({}, {'_id': 0, 'username': 1, 'message': 1, 'time': 1, 'date': 1, 'followed': 1}))
     last_date = ''
-    
     for message in messages:
         if message['date'] != last_date:
             message['new_date'] = message['date']
             last_date = message['date']
-    
+    return messages
+
+@app.route('/')
+def chat():
+    messages = fetch_messages()
     return render_template('chat.html', messages=messages)
 
 @socketio.on('connect')
@@ -41,18 +46,47 @@ def handle_disconnect():
 
 @socketio.on('message')
 def handle_message(data):
+    global last_message_details
+
     username = data.get('username', 'User')
     message = data['message']
-    
+    current_time = get_current_time()
+    current_date = get_current_date()
+
     if message.startswith('/theme'):
         theme_command = message.split(' ')[1]
         if theme_command in ['1', '2', '3']:
             emit('change_theme', theme_command, broadcast=True)
         elif theme_command == 'dark':
             emit('toggle_dark_mode', broadcast=True)
+        return
 
-    new_message = {'username': username, 'message': message, 'time': get_current_time(), 'date': get_current_date()}
-    send(new_message, broadcast=True)
+    # Check if the last message is from the same user and within a minute
+    followed = 0
+    if last_message_details['username'] == username and last_message_details['time'] == current_time:
+        followed = 1
+
+    new_message = {
+        'username': username,
+        'message': message,
+        'time': current_time,
+        'date': current_date,
+        'followed': followed
+    }
+
+    # Update last message details
+    last_message_details = {
+        'username': username,
+        'time': current_time
+    }
+
+    # Broadcast message as followed or not followed
+    if followed:
+        emit('followed_message', new_message, broadcast=True)
+    else:
+        send(new_message, broadcast=True)
+
+    # Insert message into the database
     messages_collection.insert_one(new_message)
 
 if __name__ == '__main__':
